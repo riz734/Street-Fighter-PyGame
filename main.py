6a0d595705e5e96e122c4f92998aee1341561c72
Shahbab Ahmed <shahbaba@umich.edu>
2022-12-21 01:15:19 -0500

    setting up fighter, imported assets

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+from fighter import Fighter
+
+
+pygame.init()
+
+#create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#bg image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+#draw background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image,(SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen.blit(scaled_bg,(0,0))
+
+#create two instances of fighter
+fighter1 = Fighter(200,310)
+fighter2 = Fighter(700,310)
+#game loop
+run = True
+while run:
+
+    draw_bg()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    
+    #update display
+    pygame.display.update()
+
+pygame.quit()
